65ad9e4a8dbf5ea6aa49a01abbf608116f4c0b31
Raul Lopes Braga <95510199+raullopesbraga@users.noreply.github.com>
2021-12-07 18:52:49 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -5,9 +5,18 @@ altura = float(input("Sua altura? "))
 
 imc = peso / altura ** 2
 
-print("Gerando Dados, aguarde!")
-from time import sleep
-sleep(3)   
+import time
+import sys
+print("Gerando Dados:")
+#animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+
+for i in range(len(animation)):
+    time.sleep(0.2)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+
+print("\n")
 
 print(F'Seu nome e {nome}, tem {idade} anos e {altura} de altura.')
 print(F'Seu resultado do seu IMC foi de {imc:.2F}')
